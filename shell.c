0627a0467a62309918d9e0cbd9d382027a915e46
Jack Bui <jdbui@cpp.edu>
2021-12-02 01:23:52 +0000

    add main argument

---
+++ shell.c
@@ -0,0 +1,19 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  lsh_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
+}
