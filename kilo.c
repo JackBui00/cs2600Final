7618eff72ebb3befaf7a8dd9c0c8035dce0e4c38
Jack Bui <jdbui@cpp.edu>
2021-12-04 05:48:46 +0000

    simplify main function

--- kilo.c
+++ kilo.c
@@ -69,16 +69,7 @@ int main() {
     enableRawMode();
     
     while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    read(STDIN_FILENO, &c, 1);
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) break;
-  }
+    editorProcessKeypress();
   
     return 0; 
 }
