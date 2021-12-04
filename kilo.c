a302de39a52c1f7d92a8b738f721e9b40cccc5ea
Jack Bui <jdbui@cpp.edu>
2021-12-04 05:58:32 +0000

    window size set up

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -62,15 +63,15 @@ char editorReadKey() {
   }
   return c;
 }
-/*** input ***/
-void editorProcessKeypress() {
-  char c = editorReadKey();
-  switch (c) {
-    case CTRL_KEY('q'):
-      write(STDOUT_FILENO, "\x1b[2J", 4);
-      write(STDOUT_FILENO, "\x1b[H", 3);
-      exit(0);
-      break;
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
   }
 }
 
@@ -91,6 +92,20 @@ void editorRefreshScreen() {
   editorDrawRows();
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
+
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+}
+
+
 
   
 /*** init ***/
